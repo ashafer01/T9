10c17fc7bcb760791415dfb52fa215e2ffb5497a
Alex Shafer <ashafer@pm.me>
2019-09-16 23:21:47 -0600

    clean up main a little bit

--- t9/__main__.py
+++ t9/__main__.py
@@ -8,21 +8,26 @@ from .bot import bot
 
 SPEC_CONFIG_LOCATIONS = ('config.yaml', 'config.yml')
 
+config_fn = None
+
 try:
     config_fn = sys.argv[1]
-    sys.stderr.write(f'Using config file from $1 = "{config_fn}"\n')
+    if config_fn:
+        sys.stderr.write(f'Using config file from $1 = "{config_fn}"\n')
 except IndexError:
-    config_fn = None
+    pass
 
 if not config_fn:
     try:
         config_fn = os.environ['T9_CONFIG_FILE']
-        sys.stderr.write(f'Using config file from $T9_CONFIG_FILE = "{config_fn}"\n')
+        if config_fn:
+            sys.stderr.write(f'Using config file from $T9_CONFIG_FILE = "{config_fn}"\n')
     except KeyError:
         pass
 
 if not config_fn:
     for fn in SPEC_CONFIG_LOCATIONS:
+        fn = os.path.abspath(fn)
         if os.path.exists(fn):
             config_fn = fn
             sys.stderr.write(f'Using spec config file location = "{config_fn}"\n')
@@ -33,9 +38,11 @@ if not config_fn:
     sys.stderr.write(f'Unable to locate config filename, please pass as $1, $T9_CONFIG_FILE, or use a spec location: {spec_list}\n')
     sys.exit(1)
 
+config_fn = os.path.expanduser(config_fn)
+config_fn = os.path.expandvars(config_fn)
+
 with open(config_fn) as f:
     config = yaml.safe_load(f.read())
 
 logging.config.dictConfig(config.get('logging', {'version': 1}))
-
 asyncio.run(bot(config), debug=True)
